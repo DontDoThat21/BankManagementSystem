29d5eb44032e601e31a4e5c609e3da64bf25c1f7
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-06-10 09:39:13 -0400

    Added background changing code to bank app.

--- BankManagementSystem/main.cpp
+++ BankManagementSystem/main.cpp
@@ -8,5 +8,15 @@ int main()
 {
     init_apartment();    
 
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+
+    WORD originalAttributes = consoleInfo.wAttributes;
+
+    // set the background color of the console
+    SetConsoleTextAttribute(hConsole, (consoleInfo.wAttributes & 0xF0) | BACKGROUND_BLUE | BACKGROUND_GREEN);
+    printf("Changed background color of banking app.\n");
+
     printf("Hello, world");
 }
