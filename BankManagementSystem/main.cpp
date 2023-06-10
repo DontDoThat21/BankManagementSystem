56c6a4932c5e96fdde7c2cb73aef3aa0904fec1c
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-06-10 10:06:16 -0400

    Adding default list of banking options

--- BankManagementSystem/main.cpp
+++ BankManagementSystem/main.cpp
@@ -9,8 +9,29 @@ using namespace Windows::Foundation;
 
 int main()
 {
+    char ch;
+    //int num;
+
     init_apartment();    
 
     system("color A1");
         
+    cout << "\t\t-----------------------------------------" << endl;
+    cout << "\t\t| Welcome to the Bank Management System |" << endl;
+    cout << "\t\t-----------------------------------------" << endl;
+
+    cout << endl;
+    cout << "\t           --- Main Menu ---"<<endl;
+    cout << "\t           --- 1. Create Account ---"<<endl;
+    cout << "\t           --- 2. Deposit Money ---"<<endl;
+    cout << "\t           --- 3. Withdraw Money ---"<<endl;
+    cout << "\t           --- 4. Balance Inquiry ---"<<endl;
+    cout << "\t           --- 5. All Account Holder List ---"<<endl;
+    cout << "\t           --- 6. Close an Account ---"<<endl;
+    cout << "\t           --- 7. Modify an Account ---"<<endl;
+    cout << "\t           --- 8. Exit ---"<<endl;
+    cout << endl;
+
+    cin >> ch;
+
 }
