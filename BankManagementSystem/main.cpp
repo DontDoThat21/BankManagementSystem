c66d1b48723a6644ce2a4a8450e8020ad2eed4af
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-06-10 09:36:41 -0400

    Add project files.

---
+++ BankManagementSystem/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include <Windows.h>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+    init_apartment();    
+
+    printf("Hello, world");
+}
